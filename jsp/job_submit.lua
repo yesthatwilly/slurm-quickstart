cb6345f467f290230703d358eb12e76dc0f6dbbd
William Dizon <81772355+yesthatwilly@users.noreply.github.com>
2025-10-15 12:04:07 -0700

    Add job submission and modification functions

---
+++ jsp/job_submit.lua
@@ -0,0 +1,8 @@
+-- job_submit.lua
+function slurm_job_submit(job_desc, part_list, submit_uid)
+    return slurm.SUCCESS
+end
+
+function slurm_job_modify(job_desc, job_rec, part_list, modify_uid)
+    return slurm.SUCCESS
+end
